e82467cdb71475a69bc205f33aff158ff45f07cf
Kubechi <jhomarsanchez@outlook.es>
2025-02-06 16:48:52 -0500

    Arreglo de dimensiones en FeelingCard

--- maiz_app/lib/screens/home/widgets/feeling_card.dart
+++ maiz_app/lib/screens/home/widgets/feeling_card.dart
@@ -18,56 +18,63 @@ class _FeelingCardState extends State<FeelingCard> {
     'Feliz': 'assets/icons/Happy_icon.png',
     'Euforico': 'assets/icons/Euphoric_icon.png',
   };
-  
 
-@override
-Widget build(BuildContext context) {
-  return CustomCard(
-    height: 130, // Altura reducida
-    child: Column(
-      children: [
-        const Text(
-          '¿Cómo te sientes hoy?',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF673AB7),
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+      height: 130, // Modifica esta línea para ajustar la altura
+      child: Column(
+        children: [
+          const Text(
+            '¿Cómo te sientes hoy?',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF673AB7),
+            ),
+            textAlign: TextAlign.center,
           ),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 7),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: feelings.entries.map((entry) {
-            final isSelected = selectedFeeling == entry.key;
-            
-            return GestureDetector(
-              onTap: () => setState(() => selectedFeeling = entry.key),
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: isSelected 
-                      ? const Color(0xFF673AB7).withOpacity(0.1)
-                      : Colors.transparent,
-                  shape: BoxShape.circle, // Forma circular
-                  border: Border.all(
-                    color: isSelected 
-                        ? const Color(0xFF673AB7) 
-                        : Colors.transparent,
-                    width: 2
-                  ),
-                ),
-                child: Image.asset(
-                  entry.value,
-                  width: 44, // Tamaño reducido
-                  height: 45,
-                ),
-              ),
-            );
-          }).toList(),
-        ),
-      ],
-    ),
-  );
-}
+          const SizedBox(height: 8),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              final maxWidth = constraints.maxWidth;
+              final itemWidth = (maxWidth - 50) /
+                  feelings.length; // Ajuste del ancho de cada imagen
+
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: feelings.entries.map((entry) {
+                  final isSelected = selectedFeeling == entry.key;
+
+                  return GestureDetector(
+                    onTap: () => setState(() => selectedFeeling = entry.key),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: isSelected
+                            ? const Color(0xFF673AB7).withOpacity(0.1)
+                            : Colors.transparent,
+                        shape: BoxShape.circle, // Forma circular
+                        border: Border.all(
+                          color: isSelected
+                              ? const Color(0xFF673AB7)
+                              : Colors.transparent,
+                          width: 2,
+                        ),
+                      ),
+                      child: Image.asset(
+                        entry.value,
+                        width: itemWidth - 12, // Ajuste del tamaño de la imagen
+                        height: itemWidth - 12,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
